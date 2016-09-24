963435483fb07b0e4f6eba48edab24e2380b104c
ChenJun <aaronchanlover@outlook.com>
2016-09-24 08:58:50 +0800

    init swift3.0

--- ACNoteSwiftTests/ACNoteSwiftTests.swift
+++ ACNoteSwiftTests/ACNoteSwiftTests.swift
@@ -28,9 +28,9 @@ class ACNoteSwiftTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        //self.measure {
             // Put the code you want to measure the time of here.
-        }
+        //}
     }
     
 }
